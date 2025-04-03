3353954db2da0c3909a843c0b365003105af8e89
Nickey Loots <nloots@st.noorderpoort.nl>
2025-04-03 11:08:01 +0200

    projecten toegevoegd

--- code-zero-producties/resources/views/homepageTelltales.view.php
+++
@@ -1,8 +0,0 @@
-<div class="bg-gray-100 flex items-center justify-center h-96">
-
-    <div class="text-center p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
-        <h1 class="text-2xl md:text-4xl font-bold text-gray-800 mb-4">🚧 Onder constructie 🚧</h1>
-        <p class="text-lg text-gray-600"></p>
-    </div>
-
-</div>
